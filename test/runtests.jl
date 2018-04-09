0c044243a58cd3e7e248f5535cbba384330f3a21
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-04-09 13:31:09 -0400

    stub for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,12 @@
+
 using Grabbit
 using Base.Test
 
+config = joinpath(Pkg.dir("Grabbit"), "test", "specs", "test.json")
+root = joinpath(Pkg.dir("Grabbit"), "test", "data", "7t_trt")
+
+d = Domain(root, config)
+
+layout = Layout(root, config)
+
 # write your own tests here
-@test 1 == 2
