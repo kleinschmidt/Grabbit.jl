b9368d57ee55d839f623a4618b21476f3da58442
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-04-09 10:33:11 -0400

    more notes and WIP layout parsing

--- src/Grabbit.jl
+++ src/Grabbit.jl
@@ -24,8 +24,29 @@ using DataStructures
 #
 # if you see a config file, create a new domain and compose it with the current one.
 #
-# for each entry: extract matching entities
-# 
+# for each file: extract matching entities, generate tags, and add tags to file
+#
+#
+# Querying:
+#
+# get files matching tags.
+# get files matching tags, restricted by domain
+# get files matching tags, restricted by extension
+# get entity ids/dirs matching tags.
+#
+# design considerations:
+#
+# what's the most natural data structure for representing all this information?
+# one-direction nesting (Domain [-> Domain] -> Entity -> File), or a
+# many-to-many mapping (Domain -> { Entities, Files }, Entity -> { Domain,
+# Files}, File -> { Domains, Entities }).  The first feels more...elegant
+# somehow but the second is how the python implenentation works and is a little
+# easier to reason about for querying: you just search the vector of things you
+# have to find the matching ones.  But it feels like you're missing out on some
+# of the benefits of having the _structure_: you don't have to search below
+# something where you know there's a mismatch...but then again, the way the
+# matching happens in the python one there could be a mismatch (if there's two
+# `sub-NN` where there are different `NN`s...)
 
 
 # python implementation does it this way:
@@ -35,16 +56,30 @@ using DataStructures
 # domains.  match entitites against file path.  construct File object with
 # applicable domains and matching entities, and add the file to the relevant
 # domains/entitites
+#
+# layout: entities are stored with _ID_ key ("domain.name").  when getting
+# unique values for entity, or count of files, searches by name and by id.
+# 
+# file: entities are stored with name key
+#
+# query by entity name, optionally with domain qualification (as domains kw)
+
 
 abstract type AbstractEntity end
 
-mutable struct Domain
+mutable struct Domain{E}
     name::String
     config::Dict
     root::String
     parent::Union{Domain,Void}
-    entities::Vector{<:AbstractEntity}
+    entities::Dict{String,E}
 end
+
+Domain(root::AbstractString, config::AbstractString) =
+    Domain(merge(Dict("root"=>root), JSON.parsefile(joinpath(root, config))))
+
+Base.show(io::IO, d::Domain) = println(io, "Domain $(d.name) ($(d.root))")
+
 
 mutable struct Entity <: AbstractEntity
     name::String
@@ -53,21 +88,20 @@ mutable struct Entity <: AbstractEntity
     mandatory::Bool
 end
 
-
-Domain(root::AbstractString, config::AbstractString) =
-    Domain(merge(Dict("root"=>root), JSON.parsefile(joinpath(root, config))))
-
 function Domain(config::Dict, parent=nothing)
-    @show config
-    d = Domain(config["name"],
-               config,
-               config["root"],
-               parent,
-               Entity[])
-    d.entities = [Entity(e, d) for e in config["entities"]]
+    d = Domain{Entity}(config["name"],
+                       config,
+                       config["root"],
+                       parent,
+                       Dict{String,Entity}())
+
+    for e in config["entities"]
+        e = Entity(e, d)
+        d.entities[e.name] = e
+    end
+
     return d
 end
-
 
 function Entity(config::Dict, domain::Domain)
     name = config["name"]
@@ -77,14 +111,61 @@ function Entity(config::Dict, domain::Domain)
     return e
 end
 
+Base.show(io::IO, e::Entity) = println(io, "Entity $(e.domain.name).$(e.name)")
 
 
+mutable struct File
+    path::String
+    filename::String
+    dirname::String
+    tags::Dict
+end
 
+function File(fn::AbstractString, domain::Domain, entities::Dict{String,Entity})
+    
+end
 
+mutable struct Layout
+    root::String
+    entities::OrderedDict{String,Entity}
+    domains::OrderedDict{String,Domain}
+    files::Vector{File}
+end
+
+# placeholder
+is_config(l::Layout, filename::AbstractString) = false
+is_config(l::Layout) = f -> is_config(l, f)
+
+exclude(::Layout, dir::AbstractString) = false
+include(::Layout, dir::AbstractString) = true
+
+function Layout(root::AbstractString, config::AbstractString)
+    domain = Domain(root, config)
+    entities = domain.entities
+    l = Layout(root,
+               OrderedDict{String,Entity}("$(domain.name).$k"=>v for (k,v) in entities),
+               OrderedDict(domain.name=>domain),
+               File[])
+
+    # walk dir top down
+    parsedir!(l, root, domain, entities)
+
+    return l
+end
+
+function parsedir!(layout::Layout, current, domain::Domain, entities::Dict{String,Entity})
+    contents = readdir(current)
+    dirs = [x for x in contents if isdir(joinpath(current,x))]
+    files = [x for x in contents if !isdir(joinpath(current,x))]
+
+    
+end
 
 
 export
     Domain,
-    Entity
+    Entity,
+    Layout,
+    File
 
 end # module
