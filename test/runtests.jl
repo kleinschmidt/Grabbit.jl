b1d4b58a6325448dd69dae30e349b6999d074787
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-04-11 11:39:04 -0400

    WIP tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,15 +2,30 @@
 using Grabbit
 using Base.Test
 
-config = joinpath(Pkg.dir("Grabbit"), "test", "specs", "test.json")
-root = joinpath(Pkg.dir("Grabbit"), "test", "data", "7t_trt")
 
-d = Domain(root, config)
-
-layout = Layout(root, config)
 
 # write your own tests here
-session1files = get(layout, session=1)
-basename.(get(layout, queries=Dict("type"=>["magnitude1", "bold"]), subject="01", session=1))
 
-@test !any(ismatch.(r"derivatives", dirname.(get(layout))))
+@testset "Grabbit" begin
+    config = joinpath(Pkg.dir("Grabbit"), "test", "specs", "test.json")
+    root = joinpath(Pkg.dir("Grabbit"), "test", "data", "7t_trt")
+
+    @testset "Domain" begin
+        d = Domain(root, config)
+    end
+
+    @testset "Layout" begin
+        layout = Layout(root, config)
+        session1files = get(layout, session=1)
+        basename.(get(layout, queries=Dict("type"=>["magnitude1", "bold"]), subject="01", session=1))
+
+    end
+
+
+    @testset "excludes" begin
+        layout = Layout(root, config)
+        @test !any(ismatch.(r"derivatives", dirname.(get(layout))))
+
+    end
+    
+end
