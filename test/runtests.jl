59307a796e999f6bd9f51bbd02ff0e10f9ba336c
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-04-11 13:41:55 -0400

    tests: querying

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,8 +17,28 @@ using Base.Test
     end
 
     @testset "Layout" begin
-        layout = Layout(root, config_fn)
+        layout = Layout(config)
+        
         session1files = get(layout, session=1)
+        @test all(haskey(f.tags, "session") for f in session1files)
+        @test all(f.tags["session"] == "1" for f in session1files)
+        @test unique(f.tags["subject"] for f in session1files) ==
+            [@sprintf("%02d", i) for i in 1:10]
+        @test any(!haskey(f.tags, "acquisition") for f in session1files)
+
+        @test get(layout, queries = Dict("session"=>1)) == session1files
+
+        subj12 = get(layout, subject = [1,2])
+        @test all(haskey(f.tags, "subject") for f in subj12)
+        @test unique(f.tags["subject"] for f in subj12) == ["01", "02"]
+
+        @test isempty(setdiff(subj12, vcat(get(layout, subject=1), get(layout, subject=2))))
+
+        @test subj12 == get(layout, subject = ["01", "02"])
+        @test subj12 == get(layout, subject = [1, "02"])
+        @test subj12 == get(layout, subject = [2, 1])
+        @test subj12 == get(layout, subject = [1, 2, 1])
+
         basename.(get(layout, queries=Dict("type"=>["magnitude1", "bold"]), subject="01", session=1))
 
     end
